eb3f5f630ba5b4cc2ad0b18c7fadf949cf4ded6c
Chris Montgomery <chr.montgomery@gmail.com>
2014-12-11 11:29:41 -0600

    project json for k and kpm update gulpfile with multiple clean targets

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp-help')(require('gulp'));
 var del = require('del');
 
-gulp.task('clean', 'clean build target directories', function (cb) {
+gulp.task('clean:build', 'clean build target directories', function (cb) {
   del([
     'SMPDomain/bin/**',
     'SMPDomain/obj/**'
     ], cb);
-});
+  });
+
+gulp.task('clean:test', 'clean test results', function (cb) {
+  del([
+    'TestResults/**',
+    'TestResults.xml'
+    ], cb);
+  });
+
+gulp.task('clean', 'clean all', ['clean:build', 'clean:test']);
